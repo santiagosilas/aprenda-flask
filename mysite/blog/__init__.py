04001c1df76b2f017dfc0f717aebd0e70295f633
Silas (Notebook) <santiagosilas@outlook.com>
2021-04-08 20:19:10 -0300

    fake module

--- mysite/blog/__init__.py
+++ mysite/blog/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
 def create_application():
+
     app = Flask(__name__)
 
+    app.config['DEBUG'] = True
     with app.app_context():
         from . import routes
+        
 
     return app
 
--- mysite/run_server.py
+++ mysite/run_server.py
@@ -1,8 +1,5 @@
 from blog import create_application
-
 app = create_application()
-app.debug = True
 
-# set FLASK_APP=
-#if __name__ == '__main__':
-#    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug = True)
